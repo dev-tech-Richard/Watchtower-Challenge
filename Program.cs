2fad65e5ed33dea2049c620ca936cffbf4bddb2e
skye92 <gamecoad92@gmail.com>
2023-01-25 21:22:42 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("what is the value of y? ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("what is the value of x? ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (y < 0)
+    if (x < 0)
+    {
+        Console.WriteLine("NW");
+    }
+    else if (x == 0)
+{
+    Console.WriteLine("N");
+}
+    else if (x > 0)
+{
+    Console.WriteLine("NE");
+}
+
+if (y == 0)
+    if (x < 0)
+    {
+        Console.WriteLine("W");
+    }
+    else if (x == 0)
+{
+    Console.WriteLine("!");
+}
+    else if (x > 0)
+{
+    Console.WriteLine("E");
+}
+
+if (y > 0)
+    if (x < 0)
+    {
+        Console.WriteLine("SW");
+    }
+    else if (x == 0)
+{
+    Console.WriteLine("S");
+}
+    else if (x > 0)
+{
+    Console.WriteLine("SE");
+}
